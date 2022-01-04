40b86ff1c16771582b7a420e96210865b88511f7
natlusn <n.mederbekov03@gmail.com>
2022-01-04 10:49:23 +0600

    month1

--- hack4.py
+++ hack4.py
@@ -1,8 +1,2 @@
-#4 dolar
-def float(dolarize()):
-    dollarize(123456.78901) -> "$123,456.80"
-    dollarize(-123456.7801) -> "-$123,456.78"
-    dollarize(1000000) -> "$1,000,000"
-class MoneyFmt:
-    def __init__(self, dolarize):
+print('${:,.2f}'.format(1000000.565656))
         
